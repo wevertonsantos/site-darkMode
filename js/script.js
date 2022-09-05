383691976a60e85850d153e15c63bdc6a593eb75
Weverton Santos <wever-santos@hotmail.com>
2022-09-05 12:16:33 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 let changeTheme = document.querySelector('#change')
-let texto = document.querySelector('.txtTopo')
-let img = document.querySelector('.imgDunk')
 
 function darkMode(){
     document.body.classList.toggle('dark')
